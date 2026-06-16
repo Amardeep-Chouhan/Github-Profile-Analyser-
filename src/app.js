c2616f1708409c0fa9d47a34d00b15494a241048
Amardeep-Chouhan <amarchouhan435@gmail.com>
2026-06-17 00:19:29 +0530

    Add security and parsing middleware to Express application

--- src/app.js
+++ src/app.js
@@ -12,3 +12,10 @@ dotenv.config();
 
 const app = express();
 
+// --- Security & parsing ---
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
