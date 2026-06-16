f1b102be36c8de8a53bfe4cbc740f876bb3163d9
Amardeep-Chouhan <amarchouhan435@gmail.com>
2026-06-17 00:18:14 +0530

    Initialize Express application with essential middleware and routing

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import rateLimit from "express-rate-limit";
+
+import profileRoutes from "./routes/profileRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorHandler.js";
+
+dotenv.config();
+
+const app = express();
+
